69e208c076063385306c7a90f079839337e9372b
Anton Padvalnikau <podvalnikov0@gmail.com>
2024-03-22 01:14:17 +0300

    Create custom user model

--- core/user/models.py
+++ core/user/models.py
@@ -1,5 +1,9 @@
-from django.contrib.auth.models import BaseUserManager
+import uuid
+
+from django.contrib.auth.models import (AbstractBaseUser,
+                                        BaseUserManager, PermissionsMixin)
 from django.core.exceptions import ObjectDoesNotExist
+from django.db import models
 from django.http import Http404
 
 
@@ -48,3 +52,32 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    email = models.EmailField(db_index=True, unique=True)
+
+    referral_code = models.CharField(max_length=8, unique=True, null=True, blank=True)
+
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.email}"
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"
