ce2f4846f86848bb65e59df3112db6a99b15a76c
Anton Padvalnikau <podvalnikov0@gmail.com>
2024-03-22 02:07:27 +0300

    Add new model field

--- core/user/models.py
+++ core/user/models.py
@@ -65,6 +65,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     referral_code = models.CharField(max_length=8, unique=True, null=True, blank=True)
 
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
     created = models.DateTimeField(auto_now=True)
