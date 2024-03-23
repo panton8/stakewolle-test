d881c8ad37730b705aa80465c9d0bff82fcd444c
Anton Padvalnikau <podvalnikov0@gmail.com>
2024-03-23 20:39:01 +0300

    Add ref code urls

--- core/routers.py
+++ core/routers.py
@@ -1,11 +1,12 @@
 from rest_framework import routers
 
 from core.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
-from core.user.viewsets import UserViewSet
+from core.user.viewsets import UserViewSet, ReferralCodeViewSet
 
 router = routers.SimpleRouter()
 
 router.register(r'user', UserViewSet, basename='user')
+router.register(r'referral', ReferralCodeViewSet, basename='referral')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
 router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
