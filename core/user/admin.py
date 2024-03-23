520bb864dd9d34185ced34140618c8a7808fce80
Anton Padvalnikau <podvalnikov0@gmail.com>
2024-03-23 20:36:53 +0300

    Register models in admin panel

--- core/user/admin.py
+++ core/user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from core.user.models import ReferralCode, User
 
-# Register your models here.
+
+@admin.register(ReferralCode)
+class RefCodeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'valid_until', 'creator')
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('public_id', 'username', 'first_name', 'last_name', 'email', 'referral_code', 'bonus', 'is_active', 'is_staff', 'is_superuser', 'created', 'updated')
