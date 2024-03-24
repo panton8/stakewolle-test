63f6e0ecab6e7922e61e1dd1640be2177b96ef29
Anton Padvalnikau <podvalnikov0@gmail.com>
2024-03-24 03:30:27 +0300

    Add email serializer

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from core.user.models import User, ReferralCode
+from core.user.models import User, ReferralCode, ReferralRelationship
 import random
 import string
 from django.utils import timezone
@@ -22,11 +22,10 @@ class UserSerializer(serializers.ModelSerializer):
 class ReferralCodeSerializer(serializers.ModelSerializer):
     code = serializers.CharField(read_only=True)
     valid_until = serializers.DateTimeField(read_only=True)
-    creator = serializers.CharField(read_only=True, source='user')
 
     class Meta:
         model = ReferralCode
-        fields = ['code', 'valid_until', 'creator']
+        fields = ['code', 'valid_until']
 
 
 class AddReferralCodeSerializer(serializers.ModelSerializer):
@@ -40,14 +39,41 @@ class AddReferralCodeSerializer(serializers.ModelSerializer):
         fields = ['code', 'valid_until', 'creator', 'valid_days']
 
     def create(self, validated_data):
+        user = self.context['request'].user
+
+        old_referral_code = user.referral_code
+
+        if old_referral_code:
+            old_referral_code.delete()
+
         valid_days = validated_data.pop('valid_days')
         validated_data["valid_until"] = timezone.now() + timezone.timedelta(days=valid_days)
         validated_data["code"] = self.generate_unique_referral_code()
-        validated_data["creator"] = self.context['request'].user
-        return ReferralCode.objects.create(**validated_data)
+        validated_data["creator"] = user
+
+        new_referral_code = ReferralCode.objects.create(**validated_data)
+
+        user.referral_code = new_referral_code
+        user.save()
+
+        return new_referral_code
 
     def generate_unique_referral_code(self):
         code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
         while ReferralCode.objects.filter(code=code).exists():
             code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
         return code
+
+
+class ReferralSerializer(serializers.ModelSerializer):
+    referred_user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = ReferralRelationship
+        fields = ['referred_user', 'created_at']
+        read_only_fields = ['referrer', 'referred_user', 'created_at']
+
+
+class EmailSerializer(serializers.Serializer):
+    email = serializers.EmailField(write_only=True)
+
