642c2cb60d036d5317ebf2b9fadd125f07909715
Anton Padvalnikau <podvalnikov0@gmail.com>
2024-03-24 03:29:27 +0300

    Add routers for email sending

--- core/routers.py
+++ core/routers.py
@@ -1,14 +1,17 @@
 from rest_framework import routers
 
 from core.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
-from core.user.viewsets import UserViewSet, ReferralCodeViewSet
+from core.user.viewsets import UserViewSet, ReferralCodeViewSet, ReferralViewSet, ShareRefCodeViewSet
 
 router = routers.SimpleRouter()
 
 router.register(r'user', UserViewSet, basename='user')
-router.register(r'referral', ReferralCodeViewSet, basename='referral')
+router.register(r'referral_code', ReferralCodeViewSet, basename='referral-code')
+router.register(r'referrals', ReferralViewSet, basename='referral-detail')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
 router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+router.register(r'share', ShareRefCodeViewSet, basename='share-code')
 
 urlpatterns = [*router.urls]
+
